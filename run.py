9ed77baa933c7d45a390372740e13adc08023e38
denoyey <110691104+denoyey@users.noreply.github.com>
2025-04-02 17:13:40 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,9 @@
+# Obfus-code by denoyey
+# github.com/denoyey
+
+
+import os, sys
+try:
+    __import__("compile") # Bisa menambahkan .main() --> isi sesuai fungsi yg mau dipanggil
+except Exception as e:
+    exit(str(e))
